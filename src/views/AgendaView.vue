35029ee0dce112c9e3ab58b0161d2cf8ec5355aa
adhes14 <adhemarduran@gmail.com>
2024-08-04 11:40:28 -0400

    agregar la vista Agenda

---
+++ src/views/AgendaView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AgendaView',
+  data() {
+    return {
+      titulo: 'Registro de contactos'
+    }
+  }
+}
+</script>
